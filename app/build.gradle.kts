c8cada81b2853227cc09d096bd3afa6f68cbd1b0
abhinandan-verma <abhinandanverma551@gmail.com>
2023-12-31 12:57:02 +0530

    Completed  Tutorial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -50,4 +50,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-firestore")
 }
--- app/src/main/java/com/abhicoding/firebase/MainActivity.kt
+++ app/src/main/java/com/abhicoding/firebase/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.database
-import com.google.firebase.database.getValue
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
@@ -18,9 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val textView : TextView = findViewById(R.id.text)
-        // Realtime Database Reference
+    /*    // Realtime Database Reference
         database = Firebase.database.reference
         // Writing custom objects to firebase
         val user1 = User("Abhinandan","12345678")
@@ -40,17 +35,54 @@ class MainActivity : AppCompatActivity() {
         }
         database.child("Users").addValueEventListener(pe)
 
-//        // Write  Data to Firebase
-//        database.child("Price").setValue("$1920")
-//        //  Reading data from Firebase
-//        val postListener = object : ValueEventListener{
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                val price = snapshot.value
-//                textView.text = price.toString()
-//            }
-//            override fun onCancelled(error: DatabaseError) {
+        // Write  Data to Firebase
+        database.child("Price").setValue("$1920")
+        //  Reading data from Firebase
+        val postListener = object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val price = snapshot.value
+                textView.text = price.toString()
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        }
+        database.child("Price").addValueEventListener(postListener)
+
+
+        // Initialize Firestore
+        val db = Firebase.firestore
+
+        // Creating a collection: "Users"
+        val usersCollection = db.collection("Users")
+        // Creating document
+        val user1 = hashMapOf("name" to "Abhinandan","lastname" to "Verma","DOB" to "Nov 2005")
+        val user2 = hashMapOf("name" to "Shivnandan","lastname" to "Verma","DOB" to "May 2008")
+        // Adding data to Collection
+        usersCollection.document("user1").set(user1)
+        usersCollection.document("user2").set(user2)
+
+        // Receive Documents from Firebase
+        val docRef = db.collection("Users").document("user1")
+        // Getting specific data from the collection
+//        docRef.get().addOnSuccessListener {document->
+//            if (document != null){
+//                textView.text = "${document.data?.get("name")}"
 //            }
 //        }
-//        database.child("Price").addValueEventListener(postListener)
+        //  Getting All Documents from the Collection
+        db.collection("Users").get().addOnSuccessListener {result ->
+            for (document in result){
+                Log.i("TAG","${document.data}")
+                textView.append("${document.data}")
+            }
+        }
+     */
+
+    // Updating the Data in Document
+        val db = Firebase.firestore
+
+        db.collection("Users").document("user1")
+           // .update("DOB","Nov 2005")
+            .delete()
     }
 }
