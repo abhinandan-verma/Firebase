c3d18431fb38318df83237566e3e2b4f6eac24c1
abhinandan-verma <abhinandanverma551@gmail.com>
2023-12-31 10:02:09 +0530

    Completed First Tutorial

--- app/src/main/java/com/abhicoding/firebase/MainActivity.kt
+++ app/src/main/java/com/abhicoding/firebase/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.abhicoding.firebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
+import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
@@ -13,10 +18,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val textView : TextView = findViewById(R.id.text)
         // Realtime Database Reference
         database = Firebase.database.reference
+        // Writing custom objects to firebase
+        val user1 = User("Abhinandan","12345678")
+        database.child("Users").setValue(user1)
 
-        // Write  Data to Firebase
-        database.child("Price").setValue("$1920")
+        // Reading custom objects from firebase
+        val pe = object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+              val u1  = snapshot.getValue<User>()
+                textView.text = u1.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        }
+        database.child("Users").addValueEventListener(pe)
+
+//        // Write  Data to Firebase
+//        database.child("Price").setValue("$1920")
+//        //  Reading data from Firebase
+//        val postListener = object : ValueEventListener{
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                val price = snapshot.value
+//                textView.text = price.toString()
+//            }
+//            override fun onCancelled(error: DatabaseError) {
+//            }
+//        }
+//        database.child("Price").addValueEventListener(postListener)
     }
 }
---
+++ app/src/main/java/com/abhicoding/firebase/User.kt
@@ -0,0 +1,3 @@
+package com.abhicoding.firebase
+
+data class User(val userName: String= " ", val password:String= " ")
